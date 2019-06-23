a1122e0a37fdf41f6b3748f05334e20083976e7c
ola <olaabeddayem@yahoo.com>
2019-06-23 05:03:10 +0300

    first commit

--- src/main/java/viewer/ViewerMain.java
+++ src/main/java/viewer/ViewerMain.java
@@ -3,13 +3,20 @@ package viewer;
 import controller.ControllerMain;
 import model.ModelMain;
 
+import java.util.Scanner;
+
+import static controller.ControllerMain.average;
+
 
 public class ViewerMain {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter Your Name? plz");
+        String name=input.nextLine();
        ControllerMain controllerMain=new ControllerMain();
         ModelMain modelMain=new ModelMain();
 
-            controllerMain.average(modelMain.marks());
+            controllerMain.check(average(modelMain.marks()));
 
     }
 
